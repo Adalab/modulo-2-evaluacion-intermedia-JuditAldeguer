54b46fdf6525aa3d79eac136570a90e0ff8b153d
JuditAldeguer <jud.vicens@hotmail.es>
2021-09-01 10:02:25 +0200

    js answer

--- js/main.js
+++ js/main.js
@@ -3,17 +3,34 @@
 //variables
 const btn = document.querySelector('.js_btn');
 const initialNumb = getRandomNumber(100);
-const chosenNumb = document.querySelector('js_input').innerHTML;
+const chosenNumb = document.querySelector('.js_input');
+const pista = document.querySelector('.js_pista');
 
 //functions
 function handleClickNumber(event) {
   event.preventDefault();
   console.log(`Mi número aleatorio es ${initialNumb}`);
   debugger;
+  answer();
+  numAttemps();
 }
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
+
+function answer() {
+  const chosenNumbN = parseInt(chosenNumb.value);
+  if (chosenNumbN === initialNumb) {
+    pista.innerHTML = 'Pista: Es correcto.';
+  } else if (chosenNumbN !== initialNumb) {
+    pista.innerHTML = 'Pista: NO es correcto.';
+  }
+}
+
+function numAttemps() {
+  console.log('In process');
+}
+
 //listeners
 btn.addEventListener('click', handleClickNumber);
