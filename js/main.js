7328eed6dbee82e32a292c1cd7798cd921034c87
JuditAldeguer <jud.vicens@hotmail.es>
2021-09-01 10:52:06 +0200

    counter

--- js/main.js
+++ js/main.js
@@ -5,31 +5,39 @@ const btn = document.querySelector('.js_btn');
 const initialNumb = getRandomNumber(100);
 const chosenNumb = document.querySelector('.js_input');
 const pista = document.querySelector('.js_pista');
+const attemps = document.querySelector('.js_attemps');
+let counter = 1;
 
 //functions
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
 function handleClickNumber(event) {
   event.preventDefault();
   console.log(`Mi número aleatorio es ${initialNumb}`);
-  debugger;
   answer();
-  numAttemps();
-}
-
-function getRandomNumber(max) {
-  return Math.ceil(Math.random() * max);
+  countingCliks();
 }
 
 function answer() {
   const chosenNumbN = parseInt(chosenNumb.value);
   if (chosenNumbN === initialNumb) {
-    pista.innerHTML = 'Pista: Es correcto.';
+    pista.innerHTML = 'Has ganado campeona!!!';
   } else if (chosenNumbN !== initialNumb) {
-    pista.innerHTML = 'Pista: NO es correcto.';
+    if ((chosenNumbN > initialNumb) & (chosenNumbN <= 100)) {
+      pista.innerHTML = 'Pista: Demasiado alto.';
+    } else if ((chosenNumbN < initialNumb) & (chosenNumbN >= 0)) {
+      pista.innerHTML = 'Pista: Demasiado bajo.';
+    } else {
+      pista.innerHTML = 'Pista: El número debe estar entre 1 y 100.';
+    }
   }
 }
 
-function numAttemps() {
-  console.log('In process');
+function countingCliks(event) {
+  debugger;
+  attemps.innerHTML = `Número de intentos: ${counter++}`;
 }
 
 //listeners
